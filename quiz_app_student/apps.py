60a715910a1bc4310b4c2484627354d56dd5884d
Shuvam Jaswal <shuvamjaswalshuvam@gmail.com>
2022-07-06 13:22:54 +0530

    adding two different apps

--- quiz_app_student/apps.py
+++ quiz_app_student/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuizAppConfig(AppConfig):
+class QuizAppStudentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quiz_app'
+    name = 'quiz_app_student'
